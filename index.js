e04510a0253dcb54a0be1c6192b0dfa256ba1563
inmycalcio <inmycalcio@gmail.com>
2026-04-15 20:28:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -175,3 +175,15 @@ cron.schedule('0 9 * * *', () => {
 }, {
     timezone: "Asia/Almaty"
 });
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running');
+});
+
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT);
+});
